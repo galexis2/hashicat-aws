b173e06095dda156cc4d8bdff3f1a9281e111290
galexis2 <alexis.gomolka@gmail.com>
2021-10-13 05:00:56 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
-  bucket_prefix = "${var.prefix}"
+  bucket_prefix = "alexis-test-12345"
   acl    = "private"
 
   versioning = {
